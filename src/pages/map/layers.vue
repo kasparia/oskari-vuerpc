a494894aa3eb677981a573629c4e67ff0d893629
kasparia <superkasperi@gmail.com>
2021-05-14 11:02:18 +0300

    Remove unnecessary layer visibility example

--- src/pages/map/layers.vue
+++ src/pages/map/layers.vue
@@ -30,29 +30,6 @@ channel.getAllLayers(function (layers) {
     <DocumentationLink type="request" :apiDoc="apiDocPageOpacity">Documentation for {{ requestNameOpacity }}</DocumentationLink>
 
     <RunExampleButton @click="toggleOpacity">Toggle opacity</RunExampleButton>
-    
-    <!-- MapLayerVisibilityRequest --> 
-    <h3>Show or hide a layer on map</h3>
-    <p>The request {{ requestNameVisibility }} can be used to show or hide a layer on map programmatically.</p>
-
-    <CodeSnippet>
-channel.getAllLayers(function (layers) {
-  // get the bottom layer
-  const layerId = layers[0].id;
-  const currentVisibility = layers[0].visible;
-  channel.postRequest('{{ requestNameVisibility }}', [layerId, !currentVisibility]);
-});
-    </CodeSnippet>
-
-    <DocumentationLink type="request" :apiDoc="apiDocPageVisibility">Documentation for {{ requestNameVisibility }}</DocumentationLink>
-
-    <RunExampleButton @click="toggleVisibility">Toggle visibility</RunExampleButton>
-    
-    <p>
-      {{ requestNameOpacity }} and {{ requestNameVisibility }} can also be used to control 
-      vector layers that are added to the map programmatically during runtime. 
-      Reference the layer with the same id used to add vector layer/features to map.
-    </p>
   </div>
 </template>
 
@@ -63,7 +40,6 @@ const apiDocPageRPC = 'framework/rpc';
 const requestNameOpacity = 'ChangeMapLayerOpacityRequest';
 const apiDocPageOpacity = 'mapping/mapmodule/request/changemaplayeropacityrequest.md';
 
-const requestNameVisibility = 'MapModulePlugin.MapLayerVisibilityRequest';
 const apiDocPageVisibility = 'mapping/mapmodule/request/MapModulePlugin.MapLayerVisibilityRequest.md';
 
 export default {
@@ -75,7 +51,6 @@ export default {
       apiDocPageRPC,
       requestNameOpacity,
       apiDocPageOpacity,
-      requestNameVisibility,
       apiDocPageVisibility
     }
   },
@@ -94,16 +69,6 @@ export default {
 
         me.$root.channel.postRequest(requestNameOpacity, [layerId, newOpacity]);
         me.$root.channel.log(requestNameOpacity + ' sent with parameters: ', [layerId, newOpacity]);
-      });
-    },
-    toggleVisibility () {
-      const me = this;
-      this.$root.channel.getAllLayers(function (layers) {
-        const layerId = layers[0].id;
-        const currentVisibility = layers[0].visible;
-
-        me.$root.channel.postRequest(requestNameVisibility, [layerId, !currentVisibility]);
-        me.$root.channel.log(requestNameVisibility + ' sent with parameters: ', [layerId, !currentVisibility]);
       });
     }
   },
